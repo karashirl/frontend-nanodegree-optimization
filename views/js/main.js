52fd5b6d8d97907df815932c2ffbefbdf00a7a3f
Kara Anderson <karashirl@gmail.com>
2016-06-28 14:03:27 -0400

    Refactor updatePositions function to avoid FSL

--- views/js/main.js
+++ views/js/main.js
@@ -489,9 +489,15 @@ function updatePositions() {
   window.performance.mark("mark_start_frame");
 
   var items = document.querySelectorAll('.mover');
-  for (var i = 0; i < items.length; i++) {
-    var phase = Math.sin((document.body.scrollTop / 1250) + (i % 5));
-    items[i].style.left = items[i].basicLeft + 100 * phase + 'px';
+  var scrollTop = document.body.scrollTop;
+
+  var phase = [];
+  for (var i = 0; i < 5; i++) {
+    phase[i] = Math.sin((scrollTop / 1250) + (i % 5)) * 100;
+  }
+
+  for (var i = 0; i < 24; i++) {
+    items[i].style.left = 256 * (i % 8) + phase[i % 5] + 'px';
   }
 
   // User Timing API to the rescue again. Seriously, it's worth learning.
